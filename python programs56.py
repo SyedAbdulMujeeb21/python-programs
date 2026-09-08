a1a428ebf194fc18a8c381a6efb02ebb14bb5391
SyedAbdulMujeeb21 <syedabdulmujeeb21@gmail.com>
2026-09-08 12:32:31 +0530

    Add files via upload

---
+++ python programs56.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    """Return True if n is a prime number, False otherwise."""
+    if n < 2:
+        return False
+    if n in (2, 3):
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if is_prime(num):
+        print(f"{num} is prime")
+    else:
+        print(f"{num} is not prime")
